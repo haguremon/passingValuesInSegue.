292e6c9ee3344b3d04ac4738ac5fbc3539e1cb45
Yuta iwasaki <yanyong0127@gmail.com>
2021-06-30 13:41:59 +0900

    Initial Commit

---
+++ passingValuesInSegue./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  passingValuesInSegue.
+//
+//  Created by IwasakIYuta on 2021/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
